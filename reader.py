dfb67b6aca3b5547c2ff7103addf074dcd3b033d
Jatin Kumar Bollareddy <jatinkumarbollareddy98@gmail.com>
2018-09-26 00:19:44 +0530

    Update reader.py

--- reader.py
+++ reader.py
@@ -30,31 +30,31 @@ def smooth(y, box_pts):
     return y_smooth
 
 
-cpu=[];m1=[];m3=[];m2=[];m4=[];m5=[];m6=[];m7=[];m_t=[];p=[];p_t=[];l=[];cpu_t=[];f=[];f_t=[];
+cpu=[];m1=[];m3=[];m2=[];m4=[];m5=[];m6=[];m7=[];m_t=[];p=[];p_t=[];l=[];cpu_t=[];f=[];f_t=[];    #instantiating empty lists
 with otf2.reader.open('/home/jatin/Documents/scorep-20180723_1503_576481374034/traces.otf2') as trace:
      for location, event in trace.events:
 
 
-	if type(event).__name__== "Metric" :
-		l= ("{}".format(event.metric))
+	if type(event).__name__== "Metric" :                         #extracting the metrics
+		l= ("{}".format(event.metric))                       #output of event.metric ="Encountered Metric event {'attributes': None, 'metric': MetricInstance [2], 'values': [0], 'time': 384260495649L} on Location [4294967296] at time 384260495649"
 		L=l.split(":")
 		if L[0]=="MetricInstance [8]":
-			cpu.append((event.values[0])/1000.0)
+			cpu.append((event.values[0])/1000.0)         #the number of if,elif conditions will depend on number of different  metric recorded in the trace file.
 			cpu_t.append(int(event.time))
-		elif L[0]=="MetricInstance [4]":
+		elif L[0]=="MetricInstance [4]":                     #the metrics are segregated into their respective lists based on the MetricInstance
 			f.append((event.values[0]))
-			f_t.append(int(event.time))
+			f_t.append(int(event.time))                  #the method to know the MetricInstance of a metric is discussed in the Readme file
 		elif L[0]=="MetricInstance [6]":
 			p.append((event.values[0])/100.0)
 			p_t.append(int(event.time))
-		elif L[0]=="MetricClass [0]":
+		elif L[0]=="MetricClass [0]":                        #the synchronous metrics (papi and perf metrics selected in the eviranment variable)
 			m1.append((event.values[0])); m2.append((event.values[1]));m3.append((event.values[2]));m4.append((event.values[3])); m5.append((event.values[4]));m6.append((event.values[5])); 
 			m_t.append(int(event.time))
       
 m_t=np.array(m_t); m1=np.array(m1); cpu=np.array(cpu); m2=np.array(m2); m3=np.array(m3); m4=np.array(m4); m5=np.array(m5); m6=np.array(m6); m7=np.array(m7);
-cpu_t=np.array(cpu_t); p_t=np.array(p_t); p=np.array(p); f=np.array(f);     f_t=np.array(f_t);
+cpu_t=np.array(cpu_t); p_t=np.array(p_t); p=np.array(p); f=np.array(f);     f_t=np.array(f_t);      #the arrays are converted into the numpy arrays
 
-fig, ax = plt.subplots(figsize=(20, 10))
+fig, ax = plt.subplots(figsize=(20, 10))                                                         #plotting 
 #ax1,ax2=four_scales(ax,cpu_t,smooth(cpu,2),p_t,smooth(p,2),m_t,m4,m_t,m5,'r', 'b', 'g', 'k')
 #plt.plot(f_t,f)
 #plt.show()
@@ -78,7 +78,7 @@ ax4.legend(['No. Cycles','Instructions'], loc='lower right')
 
 plt.show()
 
-workbook = xlsxwriter.Workbook('Experiment4.xlsx')
+workbook = xlsxwriter.Workbook('Experiment4.xlsx')            #an optional step if we want to export the metrics as an excel file
 worksheet = workbook.add_worksheet()
 
 worksheet.write_row('A1', ('Timestamp','m1', 'm2', 'm3', 'Page-faults', '', 'CPU-cycles','','Time stamp','CPU temperature (C)','','Time stamp','CPU Power Consumed (W)'))
